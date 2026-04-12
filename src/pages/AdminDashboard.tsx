49884647ff5b954d6943e17fb42f9e98ef4651da
DanieBad <djb.rsa@gmail.com>
2026-04-12 20:11:43 +0200

    chore: ensure AdminDashboard throws stringified JSON to catch invite API errors

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -166,7 +166,7 @@ export const AdminDashboard = () => {
         const result = await response.json();
         
         if (!response.ok) {
-            throw new Error((result.error ? result.error + ' | ' + JSON.stringify(result.full_error) : false) || 'Failed to send invite');
+            throw new Error(JSON.stringify(result));
         }
 
         alert(`Invite successfully sent to ${email}!`);
